c91aa1e1140118bfffac84c41fdbaee1feb4dfe7
Dima Gvardin <eelsovskayaboroda@gmail.com>
2026-06-05 23:47:26 +0300

    Fix new module tests

--- tests/test_track_service.py
+++ tests/test_track_service.py
@@ -19,7 +19,7 @@ from domain.models import (
 mapper и service:'''
 # Модульный тест на взаимодействие validater, mapper и service
 
-# синхронизация одного нового трека
+# синхронизация одного/нескольких нового трека
 def test_should_sync_new_track():
     # Arrange
     repository = Mock()
@@ -39,9 +39,8 @@ def test_should_sync_new_track():
                     "artist": {
                         "#text": "Artist 1"
                     },
-                    "album": {
-                        "#text": "Album 1"
-                    }
+                    "track_key": ("track 1-artist 1"
+                    )
                 }
             ]
         }     
@@ -65,8 +64,62 @@ def test_should_sync_new_track():
     # так как сохраняется только 1 трек
     repository.save.assert_called_once()
 
-# синхранизация трека без обложки
-# влияет ли на pipeline отсутствие обложки? 
+def test_should_sync_multiple_tracks():
+
+    repository = Mock()
+
+    repository.exists.return_value = (
+        False
+    )
+
+    lastfm_client = Mock()
+
+    lastfm_client.get_recent_tracks.return_value = {
+
+        "recenttracks": {
+            "track": [
+                {
+                    "name": "Track 1",
+                    "artist": {
+                        "#text": "Artist 1"
+                    }
+                },
+                {
+                    "name": "Track 2",
+                    "artist": {
+                        "#text": "Artist 2"
+                    }
+                },
+                {
+                    "name": "Track 3",
+                    "artist": {
+                        "#text": "Artist 3"
+                    }
+                }
+            ]
+        }     
+    }
+
+    service = TrackService(
+        repository,
+        lastfm_client
+    )
+
+    result = (
+        service.sync_new_track()
+    )
+
+    # Assert
+    # 3 трека должны быть сохранены в репозитории,
+    # так как они не существуют
+    assert len(result) == 3
+
+    # метод save должен быть вызван 3 раза,
+    # так как сохраняется 3 трека
+    repository.save.call_count == 3
+
+# синхранизация трека без обложки/альбома
+# влияет ли на pipeline отсутствие обложки/альбома? 
 # - нет, так как она не обязательна и
 # не влияет на остальные этапы синхронизации трека
 def test_should_sync_track_without_cover():
@@ -127,7 +180,53 @@ def test_should_sync_track_without_cover():
     
     repository.save.assert_called_once()
 
-# проверк условия возникновения ошибки Дедубликации: 
+def test_sync_track_track_without_album():
+
+    # Arrange
+    repository = Mock()
+
+    repository.exists.return_value = (
+        False
+    )
+
+    lastfm_client = Mock()
+
+    lastfm_client.get_recent_tracks.return_value = {
+        "recenttracks": {
+            "track": [
+                {
+                    "name": "Track 1",
+                    "artist": {
+                        "#text": "Artist 1"
+                    },
+                    "album": {
+                        "#text": ""
+                    },
+                }
+            ]
+        }     
+    }
+
+    service = TrackService(
+        repository,
+        lastfm_client
+    )
+    
+    # Act
+
+    result = (
+        service.sync_new_track()
+    )
+    # Assert
+    # трек должен быть сохранен в репозитории, 
+    # так как он не существует, несмотря на отсутствие названия альбома
+    assert len(result) == 1
+    
+    repository.save.assert_called_once()
+
+# Deduplication Testing Cases:
+
+# проверка условия возникновения ошибки Дедубликации: 
 # при попытке синхронизации трека, который уже существует в репозитории, 
 # должен быть вызван DuplicateTrackError
 def test_should_raise_duplicate_error():
@@ -168,6 +267,90 @@ def test_should_raise_duplicate_error():
     with pytest.raises(DuplicateTrackError):
         service.sync_new_track()
     #  проверка, что не сохранился
+    repository.save.assert_not_called()
+
+# проверка условия успешной синхронизации уникального трека
+# /несохранения существующего трека
+# (без исспользования контракта с репозиторием Last.fm, 
+# так как он не влияет на логику синхронизации трека)
+def test_should_accept_unique_track ():
+    # Arrange
+    repository = Mock()
+
+    repository.exists.return_value = (
+        False
+    )
+
+    service = TrackService(
+        repository,
+        None
+    )
+
+    track = Track(
+
+        track="Numb",
+
+        artist="Linkin Park",
+
+        album="Meteora",
+
+        track_key=(
+            "numb-linkin park"
+        ),
+
+        cover_image=None
+    )
+    
+    # Act
+
+    result = (
+        service.sync_duplicate(
+            track
+        )
+    )
+    # Assert
+    # при попытке синхронизации трека, который не существует в репозитории, 
+    # должен быть возвращен True, так как трек уникальный и может быть сохранен
+    assert result is True
+
+def test_should_not_save_duplicate_track():
+
+    repository = Mock()
+
+    repository.exists.return_value = (
+        True
+    )
+
+    service = TrackService(
+        repository,
+        None
+    )
+
+    track = Track(
+
+        track="Numb",
+
+        artist="Linkin Park",
+
+        album="Meteora",
+
+        track_key=(
+            "numb-linkin park"
+        ),
+
+        cover_image=None
+    )
+    
+    # Act & Assert
+
+    with pytest.raises(DuplicateTrackError):
+        service.sync_duplicate(
+            track
+        )
+    
+    # при попытке синхронизации трека, который уже существует в репозитории, 
+    # должен быть вызван DuplicateTrackError и не должен быть сохранен новый трек, 
+    # так как он уже существует
     repository.save.assert_not_called()
 
 ''' Дополнительные cases для тестирования взаимодействия между models и 
